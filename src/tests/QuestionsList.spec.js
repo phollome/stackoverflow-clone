4aff429809b10f2ae027bf6004d61a95eefe51cf
Peter Holló <lordpejdar@gmail.com>
2021-01-15 15:06:18 +0100

    Make test data accessible for other tests

--- src/tests/QuestionsList.spec.js
+++ src/tests/QuestionsList.spec.js
@@ -1,19 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import QuestionsList from "../components/QuestionsList";
-
-const data = {
-  questions: [
-    {
-      id: 1,
-      title: "The Question!",
-      description:
-        "The answer to the ultimate question of life, the universe and everything",
-      createAt: "Fri Jan 15 2021 10:12:19 GMT+0100",
-      author: "peter",
-    },
-  ],
-  answers: [{ id: 1, text: "42!", author: "sookie", question: 1 }],
-};
+import data from "./testData.json";
 
 test("show list with amount of questions", () => {
   render(<QuestionsList list={data.questions} />);
